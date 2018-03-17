44ca9a31f731cc34d893f73950482c234cde1da3
Jorge Andrade <jorandradefig@gmail.com>
2018-03-16 21:29:15 -0600

    Update home style

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
+const style = {
+  home: {
+    height: '100vh',
+    backgroundColor: 'lightgreen',
+    paddingTop: '30px'
+  },
+  p: {
+    color: 'red',
+    fontSize: '30px',
+    margin: '0px'
+  }
+}
+
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <p>Home</p>
+      <div className="Home" style={style.home}>
+        <p style={style.p}>Places</p>
       </div>
     )
   }
