369e0d8fedd6474dbd213dbfde21c4d30c973cef
Jorge Andrade <jorandradefig@gmail.com>
2018-03-16 21:59:13 -0600

    Add google map

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Container, Row, Col, Button } from 'reactstrap'
+import GMap from './GMap.js'
 
 const style = {
   home: {
@@ -11,6 +12,12 @@ const style = {
     color: 'red',
     fontSize: '30px',
     marginTop: '0px'
+  },
+  button: {
+
+  },
+  map: {
+
   }
 }
 
@@ -18,11 +25,17 @@ class Home extends Component {
   render() {
     return (
       <div className="Home" style={style.home}>
-        <Container>
+        <Container fluid>
           <Row>
-            <Col xs={12} className="d-flex flex-column align-items-center">
+            <Col xs={12} className="p-0 d-flex flex-column align-items-center">
               <p className="text-center" style={style.p}>Places</p>
-              <Button color={'danger'}>Add Location</Button>
+              <Button color={'danger'} className="mb-5">Add Location</Button>
+              <GMap
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ width: `100%`, height: `100%` }} />}
+                containerElement={<div style={{ width: `100%`, height: `400px` }} />}
+                mapElement={<div style={{ width: `100%`, height: `100%` }} />}
+              />
             </Col>
           </Row>
         </Container>
